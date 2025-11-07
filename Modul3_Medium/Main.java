011d3db5f2b0822b3409e2aa6f6443ac19cb495a
daelzikri <105045540+daelzikri@users.noreply.github.com>
2025-11-07 14:00:06 +0800

    Update Main.java

--- Modul3_Medium/Main.java
+++ Modul3_Medium/Main.java
@@ -20,21 +20,6 @@ public class Main {
                 daftar_pasien_1.addPasien(new Pasien(15, "Dylan Hughes", 36, 127, 82));
 
                 ListPasien daftar_pasien_2 = new ListPasien();
-                // daftar_pasien_2.addPasien(new Pasien(3, "Marcus Turner", 29, 117, 76));
-                // daftar_pasien_2.addPasien(new Pasien(8, "Madison Clark", 27, 118, 77));
-                // daftar_pasien_2.addPasien(new Pasien(12, "Abigail Foster", 30, 120, 79));
-                // daftar_pasien_2.addPasien(new Pasien(6, "Isabella Moore", 33, 125, 83));
-                // daftar_pasien_2.addPasien(new Pasien(11, "Patrick Carter", 35, 126, 82));
-                // daftar_pasien_2.addPasien(new Pasien(1, "Ethan Sullivan", 37, 128, 82));
-                // daftar_pasien_2.addPasien(new Pasien(15, "Lucas Griffin", 38, 129, 84));
-                // daftar_pasien_2.addPasien(new Pasien(7, "Jason Bennett", 41, 131, 86));
-                // daftar_pasien_2.addPasien(new Pasien(14, "Ella Simmons", 43, 134, 88));
-                // daftar_pasien_2.addPasien(new Pasien(5, "Liam Anderson", 46, 138, 90));
-                // daftar_pasien_2.addPasien(new Pasien(10, "Sophia Grant", 44, 139, 91));
-                // daftar_pasien_2.addPasien(new Pasien(2, "Chloe Ramirez", 50, 142, 94));
-                // daftar_pasien_2.addPasien(new Pasien(9, "George Ramirez", 54, 150, 97));
-                // daftar_pasien_2.addPasien(new Pasien(4, "Sophie Turner", 61, 155, 98));
-                // daftar_pasien_2.addPasien(new Pasien(13, "Nicholas Hayes", 59, 156, 101));
 
                 daftar_pasien_2.addPasien(new Pasien(1, "Ethan Sullivan", 37, 128, 82));
                 daftar_pasien_2.addPasien(new Pasien(2, "Chloe Ramirez", 50, 142, 94));
@@ -76,8 +61,8 @@ public class Main {
                 daftar_dokter.addDokter(new Dokter(3, "Dr. Jonathan Reed", "Orthopedics", 20, daftar_pasien_3));
 
 
-                // System.out.println("======  Menampilkan Seluruh Data Dokter dan Pasien  ======");
-                // daftar_dokter.displayDokterDanPasien();
+                System.out.println("======  Menampilkan Seluruh Data Dokter dan Pasien  ======");
+                daftar_dokter.displayDokterDanPasien();
                 long startTime;
                 long endTime;
                 double waktu;
@@ -116,7 +101,7 @@ public class Main {
                                 " Mengurutkan pasien dari Dr. Jonathan Reed berdasarkan Nama dengan Selection Sort ");
                 System.out.println(
                                 "===================================================================================");
-                                daftar_dokter.backupPasienData("Dr. Jonathan Reed");
+                daftar_dokter.backupPasienData("Dr. Jonathan Reed");//bakcup juga sudah ada di dalam method sort, bebas mau taruh dimana
                 startTime = System.nanoTime();
                 daftar_dokter.selectionSortPasienByNama("Dr. Jonathan Reed");
                 endTime = System.nanoTime();
@@ -128,7 +113,7 @@ public class Main {
                 System.out.println("=================================================================================");
                 System.out.println(" Mengurutkan pasien dari Dr. Jonathan Reed berdasarkan nama dengan Insertion Sort ");
                 System.out.println("=================================================================================");
-                daftar_dokter.restorePasienData("Dr. Jonathan Reed");
+                daftar_dokter.restorePasienData("Dr. Jonathan Reed");//wajib, agar data yg dirurutkan adalah data acak, bukan hasil sort sebelumnya
                 startTime = System.nanoTime();
                 daftar_dokter.insertionSortPasienByNama("Dr. Jonathan Reed");
                 endTime = System.nanoTime();
@@ -207,4 +192,5 @@ public class Main {
 
                 
         }
+
 }
